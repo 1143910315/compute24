7201a17b79a66e36f3bf75ba0c508bbd5f5e175e
林嘉豪 <1143910315@qq.com>
2016-10-22 18:14:52 +0800

    新方法

--- 二十四点猜解/Form1.cs
+++ 二十四点猜解/Form1.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿#define a
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
 namespace 二十四点猜解 {
 	public partial class Form1 : Form {
 		public Form1() {
@@ -14,6 +14,9 @@ namespace 二十四点猜解 {
 
 		private void button1_Click(object sender, EventArgs e) {
 			int i = yunsuan(5, 3, 3);
+#if a
+			caiquanjie(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text));
+#else
 			try {
 				if(caijie(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text))==true){
 					MessageBox.Show("可以通过加减乘除算出24");
@@ -23,6 +26,7 @@ namespace 二十四点猜解 {
 			}catch(Exception){
 				MessageBox.Show("请输入四个数字，程序将计算加减乘除是否可以得到24");
 			}
+#endif
 		}
 		private bool caijie(int a, int b, int c, int d) {
 			int[] num = new int[] { a, b, c, d };
@@ -66,7 +70,6 @@ namespace 二十四点猜解 {
 												}
 											}
 										}
-											//textBox5.Text += i.ToString() + " " + j.ToString() + " " + k.ToString() + " " + l.ToString() + "\r\n";
 									}
 								}
 							}
@@ -93,6 +96,82 @@ namespace 二十四点猜解 {
 				return a % b != 0 ? -1 : d;
 			}
 			return -1;
+		}
+		private bool caiquanjie(int a, int b, int c, int d) {
+			int[] num = new int[] { a, b, c, d };
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					if (j != i) {
+						for (int k = 0; k < 4; k++) {
+							if (k != i && k != j) {
+								for (int l = 0; l < 4; l++) {
+									if (l != i && l != j && l != k) {
+										for (int m = 0; m < 4; m++) {
+											int z = yunsuan(num[i], num[j], m);
+											if (z != -1) {
+												for (int n = 0; n < 4; n++) {
+													int y = yunsuan(z, num[k], n);
+													if (y != -1) {
+														y = yunsuan(num[k], z, n);
+														if (y != -1) {
+															string str = "((" + num[i].ToString() + yunsuanfu(m) + num[j].ToString() + ")" + yunsuanfu(n) + num[k].ToString() + ")";
+															if (doublecompute(y, num[l], ref str)) {
+																textBox5.Text += str + "\r\n";
+															}
+														}
+													}
+													if ((n & 1) == 1) {
+														y = yunsuan(num[k], z, n);
+														if (y != -1) {
+															string str = "(" + num[k].ToString() + yunsuanfu(n) + "(" + num[i].ToString() + yunsuanfu(m) + num[j].ToString() + ")" + ")"  ;
+															if (doublecompute(y, num[l], ref str)) {
+																textBox5.Text += str + "\r\n";
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			return false;
+		}
+		private bool doublecompute(int y, int z,ref string str) {
+			for (int o = 0; o < 4; o++) {
+				int x = yunsuan(y, z, o);
+				if (x == 24) {
+					str += yunsuanfu(o) + z.ToString();
+					return true;
+				}
+				if ((o & 1) == 1) {
+					x = yunsuan(z, y, o);
+					if (x == 24) {
+						str = z.ToString() + yunsuanfu(o)+str;
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+		private string yunsuanfu(int c) {
+			if (c == 0) {
+				return "+";
+			}
+			if (c == 1) {
+				return "-";
+			}
+			if (c == 2) {
+				return "*";
+			}
+			if (c == 3) {
+				return "/";
+			}
+			return "";
 		}
 	}
 }
